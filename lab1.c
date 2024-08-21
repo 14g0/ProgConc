f40f76fe7364d2eb8d87616c33d68eff6e92d316
Iago <iagocesarts@hotmail.com>
2024-08-20 22:53:06 -0300

    Lab1 concluído

--- lab1.c
+++ lab1.c
@@ -6,8 +6,7 @@
 /*----------------------------------------------------------------------------*/
 
 typedef struct argsUnaryInc {
-    int incStart, incQtd,
-        *numberArray;
+    int incStart, incQtd;
 } argsUnaryInc;
 
 /*----------------------------------------------------------------------------*/
@@ -18,55 +17,61 @@ pthread_t *initThreadArray(int size_arr);
 
 /*----------------------------------------------------------------------------*/
 
+int *numberArray;
+
 int main(int argc, char** argv) {
     pthread_t *threads;
-    argsUnaryInc arg;
-    int cont, supVar, N, M, *numberArray, *baseArray;
+    argsUnaryInc *arg;
+    int cont, div, mod, N, M, *baseArray;
 
-    if((argc < 2) || (argc > 3)) {
-        puts("\033[32mQuantidade de argumentos inválida [main L7]");
+    if(argc != 3) {
+        puts("\033[33mQuantidade de argumentos inválida [main L7]\033[m");
         exit(-1);
     }
+
     M = atoi(argv[1]);
     N = atoi(argv[2]);
+    if(M > N) M = N;
+    div = N / M;
+    mod = N % M;
 
     numberArray = generateIntArray(N);
     threads = initThreadArray(M);
 
     if((baseArray = malloc(sizeof(int) * N)) == NULL) {
-        puts("Não foi possível alocar o vetor de comparação [main L17]");
+        puts("\033[31mNão foi possível alocar o vetor de comparação [main L17]\033[m");
         exit(-1);
     }
     for(cont = 0 ; cont < N ; cont += 1) baseArray[cont] = numberArray[cont];
-
-    supVar = (int) N / M;
-    arg.numberArray = numberArray;
-    arg.incQtd = supVar;
+    N = 0;
 
     for(cont = 0 ; cont < M ; cont += 1) {
-        arg.incStart = cont * supVar;
-        if(pthread_create(&threads[cont], NULL, unaryIncrement, &arg)) {
-            puts("\033[31mNão foi possível instaciar uma nova thread [initThreads L3]");
+        if((arg = malloc(sizeof(argsUnaryInc))) == NULL) {
+            puts("\033[31mNão foi possível alocar um novo argumento para a thread [mallocArgsUnaryInc]\033[m");
+            exit(-3);
+        }
+
+        arg->incStart = N;
+        arg->incQtd = div + (cont < mod);
+        N += arg->incQtd;
+
+        if(pthread_create(&threads[cont], NULL, unaryIncrement, arg)) {
+            puts("\033[31mNão foi possível instaciar uma nova thread [initThreads L3]\033[m");
             exit(-13);
         }
-    }
-
-    for (cont = 0; cont < M ; cont += 1) {
-        if (pthread_join(threads[cont], NULL)) {
+        if(pthread_join(threads[cont], NULL)) {
             printf("--ERRO: pthread_join() da thread %d\n", cont); 
         }
     }
 
     for(cont = 0 ; cont < N ; cont += 1) {
-        printf("%02d ", arg.numberArray[cont]);
+        if(numberArray[cont] != baseArray[cont] + 1) {
+            printf("NA: %d, BA: %d\n", numberArray[cont], baseArray[cont]);
+            puts("\033[31mUm valor não está correto, o programa deu errado\033[m");
+            return -1;
+        }
     }
-    puts("");
-
-    for(cont = 0 ; cont < N ; cont += 1) {
-        printf("%02d ", baseArray[cont]);
-    }
-    puts("");
- 
+    puts("\033[32mArrays verificados, deu tudo certo :)\033[m");
     return 1;
 }
 
@@ -74,11 +79,11 @@ void *unaryIncrement(void *arg) {
     int cont;
     argsUnaryInc *argument = (argsUnaryInc *)arg;
 
-    printf("%d %d\n", argument->incQtd, argument->incStart);
-
     for(cont = 0 ; cont < argument->incQtd ; cont += 1) {
-        argument->numberArray[cont + argument->incStart] += 1;
+        numberArray[cont + argument->incStart] += 1;
     }
+
+    free(arg);
 
     return NULL;
 }
@@ -88,7 +93,7 @@ int *generateIntArray(int qtt) {
     srand(time(0));
 
     if((array = malloc(sizeof(int) * qtt)) == NULL) {
-        puts("\033[31mNão foi possível alocar o vetor de inteiros [generateIntArray L5]");
+        puts("\033[31mNão foi possível alocar o vetor de inteiros [generateIntArray L5]\033[m");
         exit (-3);
     }
 
@@ -101,7 +106,7 @@ pthread_t *initThreadArray(int size_arr) {
     pthread_t *threads;
 
     if((threads = malloc(sizeof(pthread_t) * size_arr)) == NULL) {
-        puts("\033[31mNão foi possível inicializar o array de threads [initThreadArray L5]");
+        puts("\033[31mNão foi possível inicializar o array de threads [initThreadArray L5]\033[m");
         exit(-25);
     }
 
