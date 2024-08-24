85a95a1814f1b875f72aa82c6c07897716258f8d
Iago Cesar <iago.souza@oceanpact.com>
2024-08-24 20:38:05 -0300

    cria a biblitoteca e separa os labs em pastas

---
+++ progConc.h
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <time.h>
+#include <string.h>
+
+/*----------------------------------------------------------------------------*/
+
+
+
+/*----------------------------------------------------------------------------*/
+
+int *generateRandNumberArray(int qtt, char* numberType);
+
+void initThread(pthread_t *threadLocal, void *threadFunction(void *arg), void* args);
+pthread_t *initThreadArray(int size_arr);
--- utils.c
+++ utils.c
@@ -1,18 +1,67 @@
 #include "./progConc.h"
 
-void initThread(pthread_t *threadArray, int threadPosition, void *threadFunction, void* args) {
-    if(pthread_create(&threadArray[threadPosition], NULL, threadFunction, args)) {
-        puts("\033[31mNão foi possível instaciar uma nova thread [initThreads L3]");
-        exit(-13);
+int *generateRandNumberArray(int qtt, char* numberType) {
+    int cont;
+    void *numberArray;
+
+    srand(time(0));
+
+    if(!strcmp(numberType, "float")) {
+        puts("entrou");
+        if((numberArray = malloc(sizeof(float) * qtt)) == NULL) {
+            puts("\033[31mNão foi possível alocar o vetor de float [generateIntArray L8]\033[m");
+            exit (-18);
+        }
+
+        for(cont = 0 ; cont < qtt ; cont += 1) {
+            ((float *)numberArray)[cont] = (float)rand() / RAND_MAX ;
+        }
+    }
+    else if(!strcmp(numberType, "int")) {
+        if((numberArray = malloc(sizeof(int) * qtt)) == NULL) {
+            puts("\033[31mNão foi possível alocar o vetor de int [generateIntArray L18]\033[m");
+            exit (-118);
+        }
+
+        for(cont = 0 ; cont < qtt ; cont += 1) {
+            ((int *)numberArray)[cont] = (rand() * 2) / qtt;
+        }
+    }
+    else if(!strcmp(numberType, "double")) {
+        if((numberArray = malloc(sizeof(double) * qtt)) == NULL) {
+            puts("\033[31mNão foi possível alocar o vetor de double [generateIntArray L28]\033[m");
+            exit (-128);
+        }
+
+        for(cont = 0 ; cont < qtt ; cont += 1) {
+            ((double *)numberArray)[cont] = (rand() * 2) / qtt;
+        }
+    }
+    else {
+        puts("Tipo de número desconhecido [generateRand]");
+        exit(-38);
+    }
+
+    return numberArray;
+}
+
+/*----------------------------------------------------------------------------*/
+
+void initThread(pthread_t *threadLocal, void *threadFunction(void *arg), void* args) {
+    if(pthread_create(threadLocal, NULL, threadFunction, args)) {
+        puts("\033[31mNão foi possível instaciar uma nova thread [initThreads L2]\033[m");
+        exit(-22);
     }
 }
+
+/*----------------------------------------------------------------------------*/
 
 pthread_t *initThreadArray(int size_arr) {
     pthread_t *threads;
 
     if((threads = malloc(sizeof(pthread_t) * size_arr)) == NULL) {
-        puts("\033[31mNão foi possível inicializar o array de threads [initThreadArray L5]");
-        exit(-25);
+        puts("\033[31mNão foi possível inicializar o array de threads [initThreadArray L4]\033[m");
+        exit(-34);
     }
 
     return threads;
